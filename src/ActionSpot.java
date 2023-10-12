930564de0279d817d42ce876491601d1ee1094c2
qsebso <quinnsebso@gmail.com>
2023-10-12 17:36:12 -0600

    Added the rest of the enum's and switch cases

--- src/ActionSpot.java
+++ src/ActionSpot.java
@@ -27,23 +27,38 @@ public class ActionSpot implements Spot {
     public void onABoardSpot(Player player) {
     	System.out.println(player.getName() + " landed on " + getName());
     	switch (actionType) {
-        	case GO:
-        		System.out.println("GO");
-        		break;
+	        case GO:
+	            System.out.println("GO");
+	            break;
+	        case PROPERTY: // NOT USED AS OF NOW
+	            System.out.println("PROPERTY");
+	            break;
 	        case CHANCE:
 	            System.out.println("CHANCE");
 	            break;
 	        case COMMUNITY_CHEST:
-	            System.out.println("COMMUNITY_CHEST");
+	            System.out.println("COMMUNITY CHEST");
+	            break;
+	        case LUXURY_TAX:
+	            System.out.println("LUXURY TAX");
+	            break;
+	        case INCOME_TAX:
+	            System.out.println("INCOME TAX");
+	            break;
+	        case RAILROAD: // NOT USED AS OF NOW
+	            System.out.println("RAILROAD");
+	            break;
+	        case UTILITY: // NOT USED AS OF NOW
+	            System.out.println("UTILITY");
 	            break;
 	        case JAIL:
 	            System.out.println("JAIL");
 	            break;
 	        case FREE_PARKING:
-	            System.out.println("FREE_PARKING");
+	            System.out.println("FREE PARKING");
 	            break;
 	        case GO_TO_JAIL:
-	            System.out.println("GO_TO_JAIL");
+	            System.out.println("GO TO JAIL");
 	            break;
 	        default:
 	            System.out.println("UNKNOWN ACTION");
@@ -55,8 +70,9 @@ public class ActionSpot implements Spot {
      * Enumerates the types of actions spots can have.
      */
     public enum ActionType {
-        GO, CHANCE, COMMUNITY_CHEST, INCOME_TAX, JAIL, FREE_PARKING, GO_TO_JAIL, LUXURY_TAX
-        //... add other types as needed
+        GO, CHANCE, COMMUNITY_CHEST, INCOME_TAX, JAIL, FREE_PARKING, GO_TO_JAIL, LUXURY_TAX, 
+        PROPERTY, RAILROAD, UTILITY
+        // add other types as needed
     }
 
 }
