386661bee45cf5a758400b2ce6d83d9cce82b6dc
igree <igree@10.0.0.252>
2023-10-16 17:52:43 -0600

    Layout for strategy (implement details later)

---
+++ src/DefaultStrategy.java
@@ -0,0 +1,20 @@
+
+public class DefaultStrategy implements Strategy{
+
+	@Override
+	public void buyProperty(Property property, int money) {
+		
+	}
+
+	@Override
+	public void mortgageUnmortgage() {
+		
+	}
+
+	@Override
+	public void bid(Property property, int money) {
+		
+	}
+
+	
+}
