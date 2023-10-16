ede5e4341dbbfcab698319e9458b31a7932666e4
huehum <117672662+huehum@users.noreply.github.com>
2023-10-16 12:11:24 -0600

    Create GameTest.java
    add load save game function

---
+++ src/GameTest.java
@@ -0,0 +1,28 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class GameTest {
+    public static void main(String[] args) throws SQLException {
+        // Establish a database connection
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/MonopolyGame", "root", "root");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        // Create an instance of GameDatabaseOperations
+        GameDatabaseOperations dbOps = new GameDatabaseOperations(conn);
+        // Call the printAllPlayers method to print all player data
+        dbOps.printAllPlayers();
+        // Create an instance of MonopolyGame and pass dbOps to the constructor
+        MonopolyGame game = new MonopolyGame(dbOps);
+
+        // Create a new Board object
+        Board board = new Board(); // Initialize the board
+        
+        // Start the Monopoly game
+        game.EntryMenu(board);
+    }
+}
