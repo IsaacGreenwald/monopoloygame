28fa7a348989834315ff02da77019dd1a6c2fb3d
huehum <117672662+huehum@users.noreply.github.com>
2023-10-16 12:09:05 -0600

    Update MonopolyGame.java
    add load save game function

--- src/MonopolyGame.java
+++ src/MonopolyGame.java
@@ -1,23 +1,125 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Scanner;
 /**
  * Represents the core Monopoly game logic and handling player setup
  */
 public class MonopolyGame {
+    private GameDatabaseOperations dbOps;
+    public MonopolyGame(GameDatabaseOperations dbOps) {
+        this.dbOps = dbOps;
+    }
+
     /**
      * Runs the Monopoly game, including player setup and game loop
+     * @throws SQLException 
      */
-    public void run() {
-        Scanner scanner = new Scanner(System.in);
+	
+	public void EntryMenu(Board board) throws SQLException {
+		Scanner scanner = new Scanner(System.in);
+		Player player = null; // Initialize player
+	    
+	    System.out.println("Welcome to Monopoly!");
+	    System.out.println("1. New Game");
+	    System.out.println("2. Load Game");
+
+	    int choice = scanner.nextInt();
+
+	    if (choice == 1) {
+	        // Start a new game
+	        run(scanner);
+	    } else if (choice == 2) {
+	        // Load a saved game
+	        loadSavedGame(scanner);
+	    } else {
+	        System.out.println("Invalid choice. Please choose again.");
+	    }
+	}
+	private Player loadSavedGame(Scanner scanner) throws SQLException {
+	    Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/MonopolyGame", "root", "root");
+
+	    // Load the game state from the database
+	    GameDatabaseOperations dbOps = new GameDatabaseOperations(conn);
+	    Player loadedPlayer = dbOps.loadFirstPlayer();
+
+	    if (loadedPlayer != null) {
+	        Board board = new Board();
+	        System.out.println("Game loaded successfully for " + loadedPlayer.getName() + loadedPlayer.getMoney());
+	        continueGameLoop(loadedPlayer, board);
+	        return loadedPlayer;
+	    } else {
+	        System.out.println("No player found in the database. Starting a new game.");
+	        run(scanner);
+	        return null;
+	    }
+	    
+	}
+
+	/**
+	 * Resumes the game loop after loading a saved game.
+	 * @param player The player to resume the game with.
+	 * @param board The game board to resume.
+	 */
+	public void continueGameLoop(Player player, Board board) {
+	    Scanner scanner = new Scanner(System.in);
+
+	    boolean gameOver = false;
+
+	    while (!gameOver) {
+	        System.out.println("\n" + player.getName() + ", it's your turn.");
+	        System.out.println("Choose an option:");
+	        System.out.println("1. Roll Dice");
+	        System.out.println("2. Mortgage/Unmortgage Property"); 
+	        System.out.println("3. Save Game");
+	        System.out.println("4. Exit");
+
+	        int choice = scanner.nextInt();
+
+	        switch (choice) {
+	            case 1:
+	                int roll = player.move(board);
+	                System.out.println(player.getName() + " rolled a total of " + roll);
+	                break;
+	            case 2:
+	                mortgageOrUnmortgageProperty(player);
+	                break;
+	            case 3:
+	                try {
+	                    dbOps.saveGame(player.getName(), player.getMoney(), player.getPosition(), board);
+	                    System.out.println("Game saved successfully!");
+	                } catch (SQLException e) {
+	                    e.printStackTrace();
+	                }
+	                break;
+
+	            case 4:
+	                System.out.println("Goodbye!");
+	                gameOver = true;
+	                break;
+	        }
+	    }
+	}
+
+
+    public void run(Scanner scanner) {
+        Scanner scanner1 = new Scanner(System.in);
 
         System.out.println("Welcome to Monopoly!");
 
         // Get player's name and chosen piece
-        String playerName = getPlayerName(scanner);
-        MonopolyPiece chosenPiece = getChosenPiece(scanner);
+        String playerName = getPlayerName(scanner1);
+        MonopolyPiece chosenPiece = getChosenPiece(scanner1);
 
-        Player player = new Player(playerName + " (" + chosenPiece.getPieceName() + ")");
-        Board board = new Board(null);
+        Player player = new Player(playerName + " (" + chosenPiece.getPieceName() + ")", 1500);
+        Board board = new Board();
+        // Insert the new player into the database
+        try {
+            dbOps.insertPlayer(player.getName(), player.getMoney(), player.getPosition());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
         boolean gameOver = false;
 
@@ -26,9 +128,10 @@ public class MonopolyGame {
             System.out.println("Choose an option:");
             System.out.println("1. Roll Dice");
             System.out.println("2. Mortgage/Unmortgage Property"); 
-            System.out.println("3. Exit");
+            System.out.println("3. Save Game"); 
+            System.out.println("4. Exit");
 
-            int choice = scanner.nextInt();
+            int choice = scanner1.nextInt();
 
             switch (choice) {
                 case 1:
@@ -38,12 +141,25 @@ public class MonopolyGame {
                 case 2:
                     mortgageOrUnmortgageProperty(player);
                     break;
-                case 3:
+                case 3:                	
+                    try {
+                        dbOps.updatePlayerMoney(player.getName(), player.getMoney());
+                        System.out.println("Player's money updated successfully!");
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }                   
+                    try {
+                        dbOps.saveGame(player.getName(), player.getMoney(), player.getPosition(), board);
+                        System.out.println("Game saved successfully!");
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+
+                case 4:
                     System.out.println("Goodbye!");
                     gameOver = true;
                     break;
-                default:
-                    System.out.println("Invalid choice. Please choose again.");
             }
         }
 
