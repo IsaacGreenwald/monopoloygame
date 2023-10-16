516c4e438d5271b14492d333fa77e433854019d2
qsebso <quinnsebso@gmail.com>
2023-10-16 01:11:57 -0600

    Changed logic so that we don't break if user input is wrong and other

--- src/MonopolyGame.java
+++ src/MonopolyGame.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 /**
  * Represents the core Monopoly game logic and handling player setup
@@ -7,47 +8,51 @@ public class MonopolyGame {
     /**
      * Runs the Monopoly game, including player setup and game loop
      */
-    public void run() {
-        Scanner scanner = new Scanner(System.in);
+	public void run() {
+	    Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Welcome to Monopoly!");
+	    System.out.println("Welcome to Monopoly!");
 
-        // Get player's name and chosen piece
-        String playerName = getPlayerName(scanner);
-        MonopolyPiece chosenPiece = getChosenPiece(scanner);
+	    // Get player's name and chosen piece
+	    String playerName = getPlayerName(scanner);
+	    MonopolyPiece chosenPiece = getChosenPiece(scanner);
 
-        Player player = new Player(playerName + " (" + chosenPiece.getPieceName() + ")");
-        Board board = new Board();
+	    Player player = new Player(playerName + " (" + chosenPiece.getPieceName() + ")");
+	    Board board = new Board();
 
-        boolean gameOver = false;
+	    boolean gameOver = false;
 
-        while (!gameOver) {
-            System.out.println("\n" + player.getName() + ", it's your turn.");
-            System.out.println("Choose an option:");
-            System.out.println("1. Roll Dice");
-            System.out.println("2. Mortgage/Unmortgage Property"); 
-            System.out.println("3. Exit");
+	    while (!gameOver) {
+	        System.out.println("\n" + player.getName() + ", it's your turn.");
+	        System.out.println("Choose an option:");
+	        System.out.println("1. Roll Dice");
+	        System.out.println("2. Mortgage/Unmortgage Property"); 
+	        System.out.println("3. Exit");
 
-            int choice = scanner.nextInt();
+	        int choice = -1; // Initialize with invalid choice
+	        if(scanner.hasNextInt()) {
+	            choice = scanner.nextInt();
+	        }
+	        scanner.nextLine();  // Consume the leftover newline character.
 
-            switch (choice) {
-                case 1:
-                    int roll = player.move(board);
-                    System.out.println(player.getName() + " rolled a total of " + roll);
-                    break;
-                case 2:
-                    mortgageOrUnmortgageProperty(player);
-                    break;
-                case 3:
-                    System.out.println("Goodbye!");
-                    gameOver = true;
-                    break;
-                default:
-                    System.out.println("Invalid choice. Please choose again.");
-            }
-        }
+	        switch (choice) {
+	            case 1:
+	                int roll = player.move(board);
+	                System.out.println(player.getName() + " rolled a total of " + roll);
+	                break;
+	            case 2:
+	                mortgageOrUnmortgageProperty(player);
+	                break;
+	            case 3:
+	                System.out.println("Goodbye!");
+	                gameOver = true;
+	                break;
+	            default:
+	                System.out.println("Invalid choice. Please choose again.");
+	        }
+	    }
+	}
 
-    }
  
    
     /**
@@ -85,16 +90,35 @@ public class MonopolyGame {
     }
 
     private String getPlayerName(Scanner scanner) {
-        System.out.print("Please enter your name: ");
-        return scanner.nextLine();
+        String name;
+        do {
+            System.out.print("Please enter your name: ");
+            name = scanner.nextLine().trim();
+            if (name.isEmpty()) {
+                System.out.println("Name cannot be empty. Please try again.");
+            }
+        } while (name.isEmpty());
+        return name;
     }
 
     private MonopolyPiece getChosenPiece(Scanner scanner) {
-        System.out.println("Choose your Monopoly piece:");
-        for (int i = 0; i < MonopolyPiece.values().length; i++) {
-            System.out.println((i + 1) + ". " + MonopolyPiece.values()[i].getPieceName());
+        int pieceChoice = -1;
+        while (pieceChoice < 1 || pieceChoice > MonopolyPiece.values().length) {
+            System.out.println("Choose your Monopoly piece:");
+            for (int i = 0; i < MonopolyPiece.values().length; i++) {
+                System.out.println((i + 1) + ". " + MonopolyPiece.values()[i].getPieceName());
+            }
+            try {
+                pieceChoice = scanner.nextInt();
+                if (pieceChoice < 1 || pieceChoice > MonopolyPiece.values().length) {
+                    System.out.println("Invalid choice. Please select a valid piece.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter a valid number.");
+                scanner.next();  // consume the invalid input
+            }
         }
-        int pieceChoice = scanner.nextInt();
-        return MonopolyPiece.getPiece(pieceChoice);
+        return MonopolyPiece.values()[pieceChoice - 1];  // Assuming MonopolyPiece.getPiece is not available
     }
+
 }
